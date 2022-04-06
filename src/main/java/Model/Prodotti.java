1aa2a15904a4b8a13e15839b40182752c22278b7
Andrea-Spagnolo <Andspagnolo7@gmail.com>
2022-04-06 12:52:20 +0200

    Add Prodotti to Model

---
+++ src/main/java/Model/Prodotti.java
@@ -0,0 +1,66 @@
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Prodotti {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String NomeProd;
+    private Long quantita;
+    private float prezzo;
+    private Date datascadenza;
+    public Prodotti(){
+    }
+    public Prodotti(String NomeProd, Long quantita, float prezzo){
+        this.NomeProd=NomeProd;
+        this.quantita=quantita;
+        this.prezzo=prezzo;
+    }
+    public Prodotti(Long id, String NomeProd, Long quantita, float prezzo, Date datascadenza){
+        this.id=id;
+        this.NomeProd=NomeProd;
+        this.quantita=quantita;
+        this.prezzo=prezzo;
+        this.datascadenza=datascadenza;
+    }
+    public Prodotti(String NomeProd, float prezzo){
+        this.NomeProd=NomeProd;
+        this.prezzo=prezzo;
+    }
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNomeProd() {
+        return NomeProd;
+    }
+    public void setNomeProd() {
+        this.NomeProd=NomeProd;
+    }
+    public Long getQuantita() {
+        return quantita;
+    }
+    public void setQuantita() {
+        this.quantita=quantita;
+    }
+    public float getPrezzo() {
+        return prezzo;
+    }
+    public void setPrezzo() {
+        this.prezzo=prezzo;
+    }
+    public Date getDatascadenza() {
+        return datascadenza;
+    }
+    public void setDatascadenza() {
+        this.datascadenza=datascadenza;
+    }
+
+}
--- src/test/java/com/devlec/ApplicazioneRestSpringEsame/ApplicazioneRestSpringEsameApplicationTests.java
+++ src/test/java/com/devlec/ApplicazioneRestSpringEsame/ApplicazioneRestSpringEsameApplicationTests.java
@@ -1,13 +1,34 @@
 package com.devlec.ApplicazioneRestSpringEsame;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootTest
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+@SpringBootApplication
 class ApplicazioneRestSpringEsameApplicationTests {
-
-	@Test
-	void contextLoads() {
+	private static final Logger logger = LoggerFactory
+			.getLogger(ApplicazioneRestSpringEsameApplicationTests.class);
+	public String Saluto;
+	public static void main(String[] args) {SpringApplication.run(ApplicazioneRestSpringEsameApplicationTests.class, args);}
+	@Bean
+	public CommandLineRunner commandLineRunner (ApplicationContext ctx) {
+		return args -> {
+			logger.debug(Saluto);
+			SimpleDateFormat simpleDateFormat =
+					new SimpleDateFormat("dd-MM-yyyy");
+			Date a = simpleDateFormat.parse("23-04-2021");
+			logger.info(a.toString());
+			Date oggi = new Date();
+			String oggiStringa = simpleDateFormat.format(oggi);
+			logger.info("Oggi e' "+oggiStringa);
+		};
 	}
-
 }
