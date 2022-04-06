2b60bd39c624b39dcaf89a3b3c4a3fd9c91f2cfd
Andrea-Spagnolo <Andspagnolo7@gmail.com>
2022-04-06 11:56:33 +0200

    Add Spring boot

---
+++ src/main/java/com/devlec/ApplicazioneRestSpringEsame/ApplicazioneRestSpringEsameApplication.java
@@ -0,0 +1,13 @@
+package com.devlec.ApplicazioneRestSpringEsame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicazioneRestSpringEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicazioneRestSpringEsameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devlec/ApplicazioneRestSpringEsame/ApplicazioneRestSpringEsameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlec.ApplicazioneRestSpringEsame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicazioneRestSpringEsameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
